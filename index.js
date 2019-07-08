7f16d44d533707d8ca7bbd0a442c78ed88e390a0
Avnish <ravi.ranjan@actiknow.com>
2019-07-08 13:02:42 +0530

    added lauout and static folder

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const app = express();
 
 const port = 8000;
 
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+
+// Extract css and javascript in layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 app.use('/', require('./routes'));
 
 // set up view engine and vie folder
