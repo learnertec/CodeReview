4e3bb097a87ee71341ccf78e0c68de601260bbc5
Avnish <ravi.ranjan@actiknow.com>
2019-07-08 11:37:39 +0530

    setup views engine and views

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 
 const port = 8000;
-    
+
 app.use('/', require('./routes'));
+
+// set up view engine and vie folder
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
