a634490b174adca88a17e4d5f9b7da874bd132bd
Avnish <r.ranjantec@gmail.com>
2019-07-23 23:07:27 +0530

    added chating functionality using socket.io

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMiddleware = require('./config/middleware');
 
+// set up chat server to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSocket = require('./config/chat_socket').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('Chat Server is listening on port 5000');
+
+
 app.use(sassMiddleware({
     src: './assets/scss',
     dest: './assets/css',
